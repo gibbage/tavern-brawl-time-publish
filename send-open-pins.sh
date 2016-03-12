5fbeebae4c2797d9ea563070236be1e77444b944
Shane Gibb <shane.gibb@gmail.com>
2016-03-12 13:25:02 +1100

    Specify an open pin so we can start adding close pins

--- send-open-pins.sh
+++ send-open-pins.sh
@@ -1,13 +1,15 @@
-curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-39 \
+curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-open-39 \
     --header "Content-Type: application/json" \
     --header "X-API-Key: SB6yu1j5t5ls4gfbmadcvljcotmmoyqq" \
     --header "X-Pin-Topics: americas" \
-    -d @pin-5pm.json
-curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-39 \
+    -d @pin-open-americas.json
+echo
+curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-open-39 \
     --header "Content-Type: application/json" \
     --header "X-API-Key: SB6yu1j5t5ls4gfbmadcvljcotmmoyqq" \
     --header "X-Pin-Topics: europe,taiwan-china,korea" \
-    -d @pin-9pm.json
+    -d @pin-open-europe_taiwan_china_korea.json
+echo
 
 # curl -X DELETE https://timeline-api.getpebble.com/v1/shared/pins/brawl-37 \
 #     --header "Content-Type: application/json" \
