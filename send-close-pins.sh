6c910dedf2d9a63e59b79e120e497e48225b74a6
Shane Gibb <shane.gibb@gmail.com>
2016-03-14 13:07:52 +1100

    Remove hardcoded sandbox Timeline API key
    Use an environment variable so I don't commit the real one.

--- send-close-pins.sh
+++ send-close-pins.sh
@@ -1,18 +1,18 @@
 curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-end-39 \
     --header "Content-Type: application/json" \
-    --header "X-API-Key: SB6yu1j5t5ls4gfbmadcvljcotmmoyqq" \
+    --header "X-API-Key: ${TIMELINE_API_KEY}" \
     --header "X-Pin-Topics: americas" \
     -d @pin-close-americas.json
 echo
 curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-end-39 \
     --header "Content-Type: application/json" \
-    --header "X-API-Key: SB6yu1j5t5ls4gfbmadcvljcotmmoyqq" \
+    --header "X-API-Key: ${TIMELINE_API_KEY}" \
     --header "X-Pin-Topics: europe" \
     -d @pin-close-europe.json
 echo
 curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-end-39 \
     --header "Content-Type: application/json" \
-    --header "X-API-Key: SB6yu1j5t5ls4gfbmadcvljcotmmoyqq" \
+    --header "X-API-Key: ${TIMELINE_API_KEY}" \
     --header "X-Pin-Topics: taiwan-china,korea" \
     -d @pin-close-taiwan-china_korea.json
 echo
--- send-open-pins.sh
+++ send-open-pins.sh
@@ -1,12 +1,12 @@
 curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-open-39 \
     --header "Content-Type: application/json" \
-    --header "X-API-Key: SB6yu1j5t5ls4gfbmadcvljcotmmoyqq" \
+    --header "X-API-Key: ${TIMELINE_API_KEY}" \
     --header "X-Pin-Topics: americas" \
     -d @pin-open-americas.json
 echo
 curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-open-39 \
     --header "Content-Type: application/json" \
-    --header "X-API-Key: SB6yu1j5t5ls4gfbmadcvljcotmmoyqq" \
+    --header "X-API-Key: ${TIMELINE_API_KEY}" \
     --header "X-Pin-Topics: europe,taiwan-china,korea" \
     -d @pin-open-europe_taiwan_china_korea.json
 echo
