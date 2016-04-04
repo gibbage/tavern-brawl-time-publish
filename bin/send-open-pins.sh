b9b4d492f968374a30975799483388bfb3642e40
Shane Gibb <shane.gibb@gmail.com>
2016-04-04 21:42:58 +1000

    Get ready for Brawl 43

--- bin/send-open-pins.sh
+++ bin/send-open-pins.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
-curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-start-americas-42 \
+curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-start-americas-43 \
     --header "Content-Type: application/json" \
     --header "X-API-Key: ${TIMELINE_API_KEY}" \
     --header "X-Pin-Topics: americas" \
     -d @data/pins/pin-open-americas.json
 echo
 
-curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-start-europe-42 \
+curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-start-europe-43 \
     --header "Content-Type: application/json" \
     --header "X-API-Key: ${TIMELINE_API_KEY}" \
     --header "X-Pin-Topics: europe" \
     -d @data/pins/pin-open-europe.json
 echo
 
-curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-start-korea-42 \
+curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-start-korea-43 \
     --header "Content-Type: application/json" \
     --header "X-API-Key: ${TIMELINE_API_KEY}" \
     --header "X-Pin-Topics: korea" \
     -d @data/pins/pin-open-korea.json
 echo
 
-curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-start-taiwan-china-42 \
+curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-start-taiwan-china-43 \
     --header "Content-Type: application/json" \
     --header "X-API-Key: ${TIMELINE_API_KEY}" \
     --header "X-Pin-Topics: taiwan-china" \
