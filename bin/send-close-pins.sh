e8d61636f567fbf6d34b73136850577b76f0e51d
Shane Gibb <shane.gibb@gmail.com>
2016-04-10 11:10:24 +1000

    Update close pins for Brawl 43

--- bin/send-close-pins.sh
+++ bin/send-close-pins.sh
@@ -1,22 +1,22 @@
-curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-end-americas-42 \
+curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-end-americas-43 \
     --header "Content-Type: application/json" \
     --header "X-API-Key: ${TIMELINE_API_KEY}" \
     --header "X-Pin-Topics: americas" \
     -d @data/pins/pin-close-americas.json
 echo
-curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-end-europe-42 \
+curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-end-europe-43 \
     --header "Content-Type: application/json" \
     --header "X-API-Key: ${TIMELINE_API_KEY}" \
     --header "X-Pin-Topics: europe" \
     -d @data/pins/pin-close-europe.json
 echo
-curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-end-taiwan-china-42 \
+curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-end-taiwan-china-43 \
     --header "Content-Type: application/json" \
     --header "X-API-Key: ${TIMELINE_API_KEY}" \
     --header "X-Pin-Topics: taiwan-china" \
     -d @data/pins/pin-close-taiwan_china.json
 echo
-curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-end-korea-42 \
+curl -X PUT https://timeline-api.getpebble.com/v1/shared/pins/brawl-end-korea-43 \
     --header "Content-Type: application/json" \
     --header "X-API-Key: ${TIMELINE_API_KEY}" \
     --header "X-Pin-Topics: korea" \
